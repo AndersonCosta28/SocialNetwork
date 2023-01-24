aadd68146c0a2def2ce16125f852d6a2f7bdfad9
Mert1s <Andgtsc@Gmail.com>
2023-01-24 08:24:22 -0300

    Removendo o module-alias

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,8 @@
-import moduleAlias from "module-alias"
-moduleAlias.addAliases({
-	"@Types": "./Types",
-	"@Providers": "./Providers"
-})
 import { setIo } from "./Providers/Websocket"
 import "reflect-metadata"
 import server from "./server"
-import redisClient from "./Providers/Redis"
+import "./Providers/Redis"
 
-await redisClient.connect()
+// await redisClient.connect()
 setIo(server)
 server.listen(3001, () => console.log("networksocial iniciado na porta 3001"))
